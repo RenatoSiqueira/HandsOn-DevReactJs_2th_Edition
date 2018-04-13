32cde5f15ede53b8611eaa5104b98df71a2127ab
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-04-13 16:32:48 -0300

    Del. Testes Iniciais com Contador

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Header from './Header'
 import Footer from './Footer'
 import Home from './Home'
 import Sobre from './Sobre'
 import Contato from './Contato'
 import Campanhas from './Campanhas'
-import base from './base'
 import Admin from './Admin'
 import Login from './Login'
 
 class App extends Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      contador: 1
-    }
-  }
-
-  componentDidMount(){
-    base.syncState('contador', {
-      context: this,
-      state: 'contador',
-      asArray: false
-    })
-  }
-
   render() {
     return (
       <Router>
