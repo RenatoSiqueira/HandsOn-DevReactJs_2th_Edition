9d36fab0206f8651fab68689b5720030dd07779e
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-04-12 22:54:11 -0300

    Campanhas

--- src/Campanhas.js
+++ src/Campanhas.js
@@ -18,6 +18,39 @@ class Campanhas extends Component{
         })
     }
 
+    renderCampanha(campanha){
+        return (
+            <section class="page-section">
+            <div class="container">
+                <div class="product-item bg-faded">
+                <div class="product-item-title d-flex">
+                    <div class="p-5 d-flex mr-auto rounded">
+                    <h2 class="section-heading mb-0">
+                        <span class="section-heading-upper">{campanha.subTitulo}</span>
+                        <span class="section-heading-lower">{campanha.nome}</span>
+                    </h2>
+                    </div>
+                </div>
+                <div class="product-item-description d-flex ml-auto">
+                    <div class="p-5 rounded">
+                    <p class="mb-0">{campanha.descricao}</p>
+                    <div class="progress">
+                        <div class="progress-bar bg-success" role="progressbar" style={{width: '25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p>Meta: R$ 5.000,00 / Atingidos: R$ 2.500,00</p>
+                    <div>
+                        <button class="btn btn-success">Contribuir</button>
+                    </div>
+                    </div>
+                </div>
+                <div class="ml-auto">
+                    
+                </div>
+                </div>
+            </div>
+            </section>            
+        )
+    }
     render(){
         return(
             <div>
@@ -41,36 +74,12 @@ class Campanhas extends Component{
                     </div>
                 </div>
                 </section>
-    
-                <section class="page-section">
-                <div class="container">
-                    <div class="product-item bg-faded">
-                    <div class="product-item-title d-flex">
-                        <div class="p-5 d-flex mr-auto rounded">
-                        <h2 class="section-heading mb-0">
-                            <span class="section-heading-upper">Delicious Treats, Good Eats</span>
-                            <span class="section-heading-lower">Bakery &amp; Kitchen</span>
-                        </h2>
-                        </div>
-                    </div>
-                    <div class="product-item-description d-flex ml-auto">
-                        <div class="p-5 rounded">
-                        <p class="mb-0">Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.</p>
-                        <div class="progress">
-                            <div class="progress-bar bg-success" role="progressbar" style={{width: '25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                        <p>Meta: R$ 5.000,00 / Atingidos: R$ 2.500,00</p>
-                        <div>
-                            <button class="btn btn-success">Contribuir</button>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="ml-auto">
-                        
-                    </div>
-                    </div>
-                </div>
-                </section>
+                { 
+                    Object
+                        .keys(this.state.campanhas)
+                        .map(key => this.renderCampanha(this.state.campanhas[key])
+                    )
+                }
             </div>
         )
     }
