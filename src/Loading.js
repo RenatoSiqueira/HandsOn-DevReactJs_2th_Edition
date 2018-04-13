18d86b2f17327cced92b66e7d6238088bdc19932
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-04-13 16:31:21 -0300

    Add. Componente Padrão de Loading

---
+++ src/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () =>{
+    return (
+        <section className='page-section'>
+            <div className='container'>
+                <p className='alert alert-info'>Aguarde...</p>
+            </div>
+        </section>
+    )
+}
+
+export default Loading
