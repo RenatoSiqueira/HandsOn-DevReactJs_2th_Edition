9f8bb1cff073080bb79a2fcbd5e25ba4161d88c7
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-04-25 00:53:03 -0300

    Add. Pagseguro platform. [in progress]

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const request = require('request-promise')
+const parse = require('xml2js').parseString
+
+const email = 'renatoelysiqueira@gmail.com'
+const token = 'FEC65CBCB0FB485C9BD4D8470B021D9B'
+const checkoutUrl = 'https://pagseguro.uol.com.br/v2/checkout/payment.html?code='
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -12,7 +19,29 @@ app.get('/', (req, res) => {
 })
 
 app.post('/donate', (req, res) => {
-    res.send(req.body)
+    request({
+        uri: 'https://ws.pagseguro.uol.com.br/v2/checkout',
+        method: 'POST',
+        form: {
+            token,
+            email,
+            currency: 'BRL',
+            itemId1: 'idCampanha',
+            itemDescription1: 'Doação',
+            itemQuantity1: '1',
+            itemAmount1: '2.00'  
+        },
+        headers: {
+            'Content-Type': 'application/x-www-urlencoded; charset=UTF-8'
+        }
+    })
+    .then( data => {
+        parse(data, (err, json) => {
+            res.send({
+                url: checkoutUrl+json.checkout.code[0]
+            })
+        })
+    })
 })
 
 exports.api = functions.https.onRequest(app)
